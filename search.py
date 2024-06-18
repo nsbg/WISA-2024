54c32eb0be9c23c1ffb9cf9e2c6d3889b74b0b94
nsbg <epdlsemgks872@gmail.com>
2024-06-18 12:50:43 +0000

    Add search.py

---
+++ search.py
@@ -0,0 +1,62 @@
+import faiss
+import numpy as np
+import pandas as pd
+
+from sentence_transformers import SentenceTransformer
+
+model_name = None
+
+def set_model_name(embed_model):
+    global model_name
+    
+    model_name = embed_model
+
+def create_vector_db():
+    global model_name
+
+    df = pd.read_csv("./dataset/vector-database/toxic_chat_unifying_category.csv")
+
+    model = SentenceTransformer(model_name, trust_remote_code=True)
+
+    if model_name is None:
+        raise ValueError("Model name has not been set. Please call set_model_name() first.")
+
+    # Create embeddings for the abnormal prompts
+    df["embedding"] = df["user_input"].apply(lambda x: model.encode(x))
+
+    # Convert embeddings to a list for later use
+    embeddings = df["embedding"].tolist()
+
+    # Initialize the Faiss index
+    dimension = model.get_sentence_embedding_dimension()
+    index = faiss.IndexFlatL2(dimension)
+
+    # Convert list of embeddings to numpy array
+    embedding_matrix = np.array(embeddings).astype('float32')
+
+    # Add embeddings to the index
+    index.add(embedding_matrix)
+
+    # Save metadata for reference
+    metadata = df[["category", "user_input", "model_output"]].to_dict(orient="records")
+
+    return index, model, metadata
+
+def find_most_similar(input_sentence, model, index, metadata, top_k=5):
+    input_embedding = model.encode(input_sentence).astype('float32').reshape(1, -1)
+
+    distances, indices = index.search(input_embedding, top_k)
+
+    results = []
+
+    for i, idx in enumerate(indices[0]):
+        result = {
+            f"top_{i+1}_category": metadata[idx]["category"],
+            f"top_{i+1}_prompt": metadata[idx]["user_input"],
+            f"top_{i+1}_output": metadata[idx]["model_output"],
+            f"top_{i+1}_similarity_score": distances[0][i]
+        }
+        
+        results.append(result)
+
+    return results
