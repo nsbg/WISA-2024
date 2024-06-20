cca1bd0c55970e054a1ba36f5b794a3a69f16de5
nsbg <epdlsemgks872@gmail.com>
2024-06-20 12:29:37 +0000

    Simplify main function

--- run.py
+++ run.py
@@ -4,16 +4,13 @@ import sys
 from process import *
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python3 run.py <file_name or text> <embedding_model_name>")
-    else:
-        user_input = sys.argv[1]
-        model_name = sys.argv[2]
+    user_input = sys.argv[1]
+    model_name = sys.argv[2]
 
-        if os.path.isfile(user_input):
-            process_file(user_input, model_name)
-        else:
-            process_text(user_input, model_name)
+    if os.path.isfile(user_input):
+        process_file(user_input, model_name)
+    else:
+        process_text(user_input, model_name)
 
 if __name__ == "__main__":
     main()
