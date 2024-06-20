15244e9f1f653ed20b1625bfa7ef0997e2780e1b
nsbg <epdlsemgks872@gmail.com>
2024-06-20 15:15:26 +0000

    Make process_text fuction result shown in terminal

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ def main(filename_or_text, model_name):
     if os.path.isfile(filename_or_text):
         process_file(filename_or_text, model_name)
     else:
-        process_text(filename_or_text, model_name)
+        print(process_text(filename_or_text, model_name))
 
 if __name__ == "__main__":
     user_input = sys.argv[1]
